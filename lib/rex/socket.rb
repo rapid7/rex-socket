d033f8de2ab5d0de77f97cb8857cc2fc4978ae8c
RageLtMan <rageltman [at] sempervictus>
2016-12-11 02:39:41 -0500

    Move peerinfo and localinfo from tcp to socket
    With the addition of UDP sessions, this information needs to be
    available to both L4 consumers. Technically speaking, socket.rb
    can present an L3-only construct (raw IP sockets), which wont have
    port information. However, this has not been an issue when using
    the new transports in testing.
    
    This is a breakout of the UDP handlers and payloads PR (#6035).

--- lib/rex/socket.rb
+++ lib/rex/socket.rb
@@ -746,6 +746,24 @@ module Socket
   end
 
   #
+  # Returns peer information (host + port) in host:port format.
+  #
+  def peerinfo
+    if (pi = getpeername_as_array)
+      return pi[1] + ':' + pi[2].to_s
+    end 
+  end 
+
+  #
+  # Returns local information (host + port) in host:port format.
+  #
+  def localinfo
+    if (pi = getlocalname)
+      return pi[1] + ':' + pi[2].to_s
+    end 
+  end 
+
+  #
   # Returns a string that indicates the type of the socket, such as 'tcp'.
   #
   def type?
