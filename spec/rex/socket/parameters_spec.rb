468157036aea2c3179b851fa850f925c6248e6dd
Spencer McIntyre <zeroSteiner@gmail.com>
2020-02-17 13:03:33 -0500

    Add some tests for the Parameters class

---
+++ spec/rex/socket/parameters_spec.rb
@@ -0,0 +1,62 @@
+# -*- coding:binary -*-
+require 'rex/socket/parameters'
+
+RSpec.describe Rex::Socket::Parameters do
+
+  let(:args) { { } }
+  subject(:params) { described_class.new(args) }
+
+  it { is_expected.to respond_to(:localhost) }
+  it { is_expected.to respond_to(:localport) }
+  it { is_expected.to respond_to(:client?) }
+  it { is_expected.to respond_to(:server?) }
+  it { is_expected.to respond_to(:ssl?) }
+  it { is_expected.to respond_to(:v6?) }
+
+  describe '.new' do
+
+    it "should handle an IPv4 local host definition" do
+      params = Rex::Socket::Parameters.new({ "LocalHost" => "1.2.3.4", "LocalPort" => 1234 })
+      expect(params.localhost).to eq ("1.2.3.4")
+      expect(params.localport).to eq 1234
+      expect(params.v6?).to eq false
+    end
+
+    it "should handle an IPv6 local host definition" do
+      params = Rex::Socket::Parameters.new({ "LocalHost" => "::1", "LocalPort" => 1234, "IPv6" => true })
+      expect(params.localhost).to eq ("::1")
+      expect(params.localport).to eq 1234
+      expect(params.v6?).to eq true
+    end
+
+  end
+
+  describe '#merge_hash' do
+    it "should handle merging hash options" do
+      expect(params.localhost).to eq "0.0.0.0"
+      expect(params.localport).to eq 0
+      new_params = params.merge_hash({"LocalHost" => "5.6.7.8", "LocalPort" => 5678 })
+      expect(new_params.localhost).to eq "5.6.7.8"
+      expect(new_params.localport).to eq 5678
+    end
+
+    it "should handle new proxy definitions" do
+      expect(params.proxies).to eq nil
+      new_params = params.merge_hash({"Proxies" => "1.2.3.4:1234, 5.6.7.8:5678"})
+      expect(new_params.proxies).to eq [
+        ["1.2.3.4", "1234"],
+        ["5.6.7.8", "5678"]
+      ]
+    end
+  end
+
+  describe '#to_hash' do
+    it "should return a populated hash" do
+      params_hash = params.to_hash
+      expect(params_hash).to be_a(Hash)
+      expect(params_hash.length).to be > 0
+      expect(params_hash).to include("LocalHost", "LocalPort", "IPv6", "SSL", "Server")
+    end
+  end
+
+end
