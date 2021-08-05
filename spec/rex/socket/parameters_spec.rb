7d158a6bc8bcdfabb241bd18bf5c5076206accb5
Grant Willcox <gwillcox@rapid7.com>
2021-08-05 14:45:20 -0500

    Add some final RuboCop fixes to spec/rex/socket/parameters_spec.rb

--- spec/rex/socket/parameters_spec.rb
+++ spec/rex/socket/parameters_spec.rb
@@ -3,7 +3,7 @@ require 'rex/socket/parameters'
 
 RSpec.describe Rex::Socket::Parameters do
 
-  let(:args) { { } }
+  let(:args) { {} }
   subject(:params) { described_class.new(args) }
 
   it { is_expected.to respond_to(:localhost) }
@@ -57,24 +57,24 @@ RSpec.describe Rex::Socket::Parameters do
       end
     end
 
-    context "given no value for IPv6" do
+    context 'given no value for IPv6' do
       [
-        {'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => '::1' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => 'localhost' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => nil }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => '::1', 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => '::1', 'PeerHost' => '::1' }, 'ExpectedResult' => true },
-        {'Options' => { 'LocalHost' => '::1', 'PeerHost' => 'localhost' }, 'ExpectedResult' => true },
-        {'Options' => { 'LocalHost' => '::1', 'PeerHost' => nil }, 'ExpectedResult' => true },
-        {'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => '::1' }, 'ExpectedResult' => true },
-        {'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => 'localhost' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => nil }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => nil, 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => nil, 'PeerHost' => '::1' }, 'ExpectedResult' => true },
-        {'Options' => { 'LocalHost' => nil, 'PeerHost' => 'localhost' }, 'ExpectedResult' => false },
-        {'Options' => { 'LocalHost' => nil, 'PeerHost' => nil }, 'ExpectedResult' => false }
+        { 'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => '::1' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => 'localhost' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => '127.0.0.1', 'PeerHost' => nil }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => '::1', 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => '::1', 'PeerHost' => '::1' }, 'ExpectedResult' => true },
+        { 'Options' => { 'LocalHost' => '::1', 'PeerHost' => 'localhost' }, 'ExpectedResult' => true },
+        { 'Options' => { 'LocalHost' => '::1', 'PeerHost' => nil }, 'ExpectedResult' => true },
+        { 'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => '::1' }, 'ExpectedResult' => true },
+        { 'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => 'localhost' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => 'localhost', 'PeerHost' => nil }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => nil, 'PeerHost' => '127.0.0.1' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => nil, 'PeerHost' => '::1' }, 'ExpectedResult' => true },
+        { 'Options' => { 'LocalHost' => nil, 'PeerHost' => 'localhost' }, 'ExpectedResult' => false },
+        { 'Options' => { 'LocalHost' => nil, 'PeerHost' => nil }, 'ExpectedResult' => false }
       ].each do |test|
         options = test['Options']
         it "should automatically set v6 to #{test['ExpectedResult']} when localhost is #{options['LocalHost'].inspect} and peerhost is #{options['PeerHost'].inspect}" do
@@ -86,41 +86,41 @@ RSpec.describe Rex::Socket::Parameters do
   end
 
   describe '#merge' do
-    it "should handle merging" do
-      new_params = params.merge(Rex::Socket::Parameters.new({"LocalHost" => "5.6.7.8", "LocalPort" => 5678 }))
-      expect(params.localhost).to eq "0.0.0.0"
+    it 'should handle merging' do
+      new_params = params.merge(Rex::Socket::Parameters.new({ 'LocalHost' => '5.6.7.8', 'LocalPort' => 5678 }))
+      expect(params.localhost).to eq '0.0.0.0'
       expect(params.localport).to eq 0
-      expect(new_params.localhost).to eq "5.6.7.8"
+      expect(new_params.localhost).to eq '5.6.7.8'
       expect(new_params.localport).to eq 5678
     end
 
-    it "should handle merging hash options" do
-      expect(params.localhost).to eq "0.0.0.0"
+    it 'should handle merging hash options' do
+      expect(params.localhost).to eq '0.0.0.0'
       expect(params.localport).to eq 0
-      new_params = params.merge({"LocalHost" => "5.6.7.8", "LocalPort" => 5678 })
-      expect(params.localhost).to eq "0.0.0.0"
+      new_params = params.merge({ 'LocalHost' => '5.6.7.8', 'LocalPort' => 5678 })
+      expect(params.localhost).to eq '0.0.0.0'
       expect(params.localport).to eq 0
-      expect(new_params.localhost).to eq "5.6.7.8"
+      expect(new_params.localhost).to eq '5.6.7.8'
       expect(new_params.localport).to eq 5678
     end
 
-    it "should handle new proxy definitions" do
+    it 'should handle new proxy definitions' do
       expect(params.proxies).to eq nil
-      new_params = params.merge({"Proxies" => "1.2.3.4:1234, 5.6.7.8:5678"})
+      new_params = params.merge({ 'Proxies' => '1.2.3.4:1234, 5.6.7.8:5678' })
       expect(params.proxies).to eq nil
       expect(new_params.proxies).to eq [
-        ["1.2.3.4", "1234"],
-        ["5.6.7.8", "5678"]
+        ['1.2.3.4', '1234'],
+        ['5.6.7.8', '5678']
       ]
     end
   end
 
   describe '#merge!' do
-    it "should handle merging in place" do
-      expect(params.localhost).to eq "0.0.0.0"
+    it 'should handle merging in place' do
+      expect(params.localhost).to eq '0.0.0.0'
       expect(params.localport).to eq 0
-      params.merge!(Rex::Socket::Parameters.new({"LocalHost" => "5.6.7.8", "LocalPort" => 5678 }))
-      expect(params.localhost).to eq "5.6.7.8"
+      params.merge!(Rex::Socket::Parameters.new({ 'LocalHost' => '5.6.7.8', 'LocalPort' => 5678 }))
+      expect(params.localhost).to eq '5.6.7.8'
       expect(params.localport).to eq 5678
     end
   end
